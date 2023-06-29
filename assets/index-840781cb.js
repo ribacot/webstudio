cdc08c55c0785ce3c1f3f73096dc0ebc1e6a15a1
ribacot <122934616+ribacot@users.noreply.github.com>
2023-06-29 22:49:58 +0000

    Deploying to gh-pages from @ ribacot/webstudio@7a490dded3b8ec11d4af7f5e18c07909416644b8 🚀

---
+++ assets/index-840781cb.js
@@ -0,0 +1,1 @@
+import"./my-9165f8f9.js";(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o);function o(){e.modal.classList.toggle("is-hidden")}})();
